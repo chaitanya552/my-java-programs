4c179840c8e6e56a54a1e5cc45fe00302c623cd7
Chaitanya Kumar Neeruonda <chaitu10552@gmail.com>
2017-12-30 18:41:58 -0500

    Update Hashmaps.java
    added comments

--- Hashmaps.java
+++ Hashmaps.java
@@ -6,13 +6,18 @@ public class Hashmaps{
     public static void main(String []argh){
     	Map<String,Integer> myMap = new HashMap<String,Integer>();
     	Scanner in = new Scanner(System.in);
+    	System.out.println("enter number of contacts");
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
+        	System.out.println("enter contact name");
             String name = in.next();
+            System.out.println("enter the above contact's phone number");
             int phone = in.nextInt();
             // Write code here
             myMap.put(name,phone);
         }
+        System.out.println("now you can search");
+        System.out.println("enter contact name to be searched");
         while(in.hasNext()){
             String s = in.next();
             // Write code here
