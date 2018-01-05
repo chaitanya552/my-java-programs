b9986006a6de198f14a3f38db90b17515b964c1f
chaitanya552 <chaitu10552@gmail.com>
2018-01-05 00:45:21 -0500

    12- inheritance

--- HackerRank_30-days-ofCode/Solution.java
+++ HackerRank_30-days-ofCode/Solution.java
@@ -2,108 +2,101 @@ package HackerRank;
 
 import java.util.Scanner;
 
-
 class Person {
 	protected String firstName;
 	protected String lastName;
 	protected int idNumber;
-	/*//default constructor
-	Person(){	
-	}*/
+
+	/*
+	 * //default constructor Person(){ }
+	 */
 	// Constructor
-	
-	Person(String firstName, String lastName, int identification){
+
+	Person(String firstName, String lastName, int identification) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = identification;
 	}
-	
+
 	// Print person data
-	public void printPerson(){
-		 System.out.println(
-				"Name: " + lastName + ", " + firstName 
-			+ 	"\nID: " + idNumber); 
+	public void printPerson() {
+		System.out.println("Name: " + lastName + ", " + firstName + "\nID: "
+				+ idNumber);
 	}
-	 
+
 }
-class Student extends Person{
+
+class Student extends Person {
 	private int[] testScores;
-    
-	
-    /*	
-    *   Class Constructor
-    *   
-    *   @param firstName - A string denoting the Person's first name.
-    *   @param lastName - A string denoting the Person's last name.
-    *   @param id - An integer denoting the Person's ID number.
-    *   @param scores - An array of integers denoting the Person's test scores.
-    */
-    // Write your constructor here
 
+	/*
+	 * Class Constructor
+	 * 
+	 * @param firstName - A string denoting the Person's first name.
+	 * 
+	 * @param lastName - A string denoting the Person's last name.
+	 * 
+	 * @param id - An integer denoting the Person's ID number.
+	 * 
+	 * @param scores - An array of integers denoting the Person's test scores.
+	 */
+	// Write your constructor here
 
+	/* Concept
+	 *  * Note: If a superclass does not have a default constructor, any
+	 * subclasses extending it must make an explicit(i.e use super->JVM takes
+	 * care) call to one of the superclass' parameterized constructors.
+	 */
+	Student(String firstName, String lastName, int identification,
+			int[] testScores) {
+		super(firstName, lastName, identification);
 
-/* * Note: If a superclass does not have a default constructor, 
- * any subclasses extending it must make an explicit(i.e use super->JVM takes care) call to one of the superclass' parameterized constructors. 
-*/ 
-	Student(String firstName, String lastName, int identification, int[] testScores) {
-    super(firstName, lastName, identification);
+		this.testScores = testScores;
+	}
 
-    this.testScores = testScores;
-}
-	//if there had been default person constructor then below works which is called implicitcal->i.e programmer directly links
-	/*Student(String firstName, String lastName, int identification, int[] testScores)
-	{
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = identification;
-		this.testScores=testScores;
-	}*/
-	
-	
-	
-    /*	
-    *   Method Name: calculate
-    *
-    *   @return A character denoting the grade.
-    */
-    // Write your method here
-	public String calculate(){
-		int sum=0;
-		float avg=0;
-		String grade =new String();
-		for(int i = 0; i < testScores.length; i++){
-			sum+=testScores[i];
+	// if there had been default person constructor then below works which is
+	// called implicit call->i.e programmer directly links. example is given below:
+	/*
+	 * Student(String firstName, String lastName, int identification, int[]
+	 * testScores) { this.firstName = firstName; this.lastName = lastName;
+	 * this.idNumber = identification; this.testScores=testScores; }
+	 */
+
+	/*
+	 * Method Name: calculate
+	 * 
+	 * @return A character denoting the grade.
+	 */
+	// Write your method here
+	public String calculate() {
+		int sum = 0;
+		float avg = 0;
+		String grade = new String();
+		for (int i = 0; i < testScores.length; i++) {
+			sum += testScores[i];
 		}
-		avg=sum/testScores.length;
-		int avgg= (int)avg;
-		
-			if (avgg>=90 && avg <=100)
-				grade="O";
-				
-		
-			if (avgg>=80 && avg <90)
-				grade="E";
-				
-	
-			if (avgg>=70 && avg <80)
-				grade="A";
-				
-		
-			if (avgg>=55 && avg <70)
-				grade="P";
-				
-		
-			if (avgg>=40 && avg <55)
-				grade="A";
-				
-		
-		
-			if (avgg<40)
-				grade="T";
-			return grade;
-		
-		
-			
+		avg = sum / testScores.length;
+
+		if (avg >= 90 && avg <= 100) {
+			grade = "O";
+		} else if (avg >= 80 && avg < 90) {
+			grade = "E";
+
+		} else if (avg >= 70 && avg < 80) {
+			grade = "A";
+
+		} else if (avg >= 55 && avg < 70) {
+			grade = "P";
+
+		} else if (avg >= 40 && avg < 55) {
+			grade = "A";
+		}
+
+		else if (avg < 40) {
+			grade = "T";
+		}
+		return grade;
+
 	}
 }
 
@@ -115,11 +108,11 @@ class Solution {
 		int id = scan.nextInt();
 		int numScores = scan.nextInt();
 		int[] testScores = new int[numScores];
-		for(int i = 0; i < numScores; i++){
+		for (int i = 0; i < numScores; i++) {
 			testScores[i] = scan.nextInt();
 		}
 		scan.close();
-		
+
 		Student s = new Student(firstName, lastName, id, testScores);
 		s.printPerson();
 		System.out.println("Grade: " + s.calculate());
