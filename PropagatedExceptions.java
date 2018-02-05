e76006a90e667ddf3602f1e353f39ce9a1f9942f
chaitanya552 <chaitu10552@gmail.com>
2018-02-04 23:51:37 -0500

    propagated exceptions

---
+++ PropagatedExceptions.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class PropagatedExceptions {
+
+	 public static void main(String[] args) {
+		    
+	        Scanner in = new Scanner(System.in);
+	        int t = in.nextInt();
+	        while (t-- > 0) {
+	        
+	            int n = in.nextInt();
+	            int p = in.nextInt();
+	            Calculator myCalculator = new Calculator();
+	            try {
+	                int ans = myCalculator.power(n, p);
+	                System.out.println(ans);
+	            }
+	            catch (Exception e) {
+	                System.out.println(e.getMessage());
+	            }
+	        }
+	        in.close();
+	    }
+	}
+class Calculator
+{
+	int n;
+	int p;
+	
+    int power(int n, int p) throws Exception
+    {
+        int result=1;
+        while(p!=0)
+        {
+            result= result*n;
+        }
+        
+        if (result <0 || n<0 || p<0)
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+        return result;
+    }
+}
