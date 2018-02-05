d4d0379124b6a04c51a2a86f77711ac772d59a2c
chaitanya552 <chaitu10552@gmail.com>
2018-02-04 23:47:45 -0500

    check if valid parenthesis

---
+++ ValidParentheses_LeetCode.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+
+public class ValidParentheses_LeetCode {
+	public static boolean balancedParenthensies(String s) {
+        Stack<Character> stack  = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '[' || c == '(' || c == '{' ) {     
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }           
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println(balancedParenthensies("{(a,b)}"));
+	        System.out.println(balancedParenthensies("{(a},b)"));
+	        System.out.println(balancedParenthensies("{)(a,b}"));
+	}
+}
+	
+    
