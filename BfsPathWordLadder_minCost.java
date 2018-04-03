35a2c245afab6ccb3ef5fb8d7b8fa20f3c03f62b
chaitanya552 <chaitu10552@gmail.com>
2018-04-02 17:14:53 -0700

    add char and anagrams logic added

--- BfsPathWordLadder_minCost.java
+++ BfsPathWordLadder_minCost.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -9,14 +10,20 @@ import java.util.Set;
 public class BfsPathWordLadder_minCost {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//String[] arr = { "hot", "hip", "hop", "pop", "dot", "kit", "kip", "mip" };
-		//String[] arr = {"appse","appls","mppse","ppse","apply","appy","app","appl","aple","apl"};
-		String[] arr = {"health","heath","heats","hents","hends"};
+		// String[] arr = { "hot", "hip", "hop", "pop", "dot", "kit", "kip",
+		// "mip" };
+		// mod check String[] arr =
+		// {"appse","appls","mppse","ppse","apply","appy","app","appl","aple","apl"};
+		// del check String[] arr = {"health","heath","heats","hents","hends"};
+		// anaagram String[] arr = { "appo", "app", "pp", "ppo", "ppoy", "appl" };
+		String[] arr={"team","mate","tea","mat","ate","aten"};
 		Set<String> wordList = new HashSet<String>(Arrays.asList(arr));
 
-		//System.out.println(findLadders("pop", "hit", wordList));
-	//	System.out.println(findLadders("ap", "apple", wordList));
-		System.out.println(findLadders("hands", "health", wordList));
+		// System.out.println(findLadders("pop", "hit", wordList));
+		// System.out.println(findLadders("ap", "apple", wordList));
+		// System.out.println(findLadders("hands", "health", wordList));
+		//System.out.println(findLadders("ppoye", "app", wordList));
+		System.out.println(findLadders("aten", "tea", wordList));
 	}
 
 	public static List<List<String>> findLadders(String endWord,
@@ -56,7 +63,7 @@ public class BfsPathWordLadder_minCost {
 
 	private static void addNeighbours(Queue<Node> queue, Set<String> visited,
 			Set<String> unvisited, Node current) {
-		System.out.println(current.val+" current");
+		System.out.println(current.val + " current");
 		char[] chars = current.val.toCharArray();
 		// edit
 		for (int i = 0; i < chars.length; ++i) {
@@ -73,9 +80,9 @@ public class BfsPathWordLadder_minCost {
 			}
 		}
 		// delete
-		
+
 		String str = current.val;
-		System.out.println(str+ " str");
+		System.out.println(str + " str");
 		for (int i = 0; i < chars.length; i++) {
 			StringBuilder sb = new StringBuilder();
 			String left = str.substring(0, i);
@@ -83,14 +90,74 @@ public class BfsPathWordLadder_minCost {
 			sb.append(left);
 			sb.append(right);
 			String delWord = sb.toString();
-			System.out.println(delWord+" del");
+			System.out.println(delWord + " del");
 			if (unvisited.contains(delWord)) {
 				System.out.println("no");
 				queue.add(new Node(delWord, current, current.dist + 1));
 				visited.add(delWord);
 			}
 		}
+		// add
+		for (int i = 0; i < str.length() + 1; ++i) {
+			for (char c = 'a'; c <= 'z'; ++c) {
+				// char tmp = chars[i];
+				// chars[i] = c;
+				StringBuilder sb = new StringBuilder();
+				String left = str.substring(0, i);
+				String right = str.substring(i, str.length());
+				if (i == 0) {
+					sb.append(c);
+					sb.append(left);
+					sb.append(right);
+				} else if (i < str.length()) {
 
+					sb.append(left);
+					sb.append(c);
+					sb.append(right);
+				} else {
+					sb.append(left);
+					sb.append(right);
+					sb.append(c);
+				}
+				String addWord = sb.toString();
+				if (unvisited.contains(addWord)) {
+					System.out.println("no");
+					queue.add(new Node(addWord, current, current.dist + 1));
+					visited.add(addWord);
+				}
+			}
+		}
+		// anagrams
+		List<Character> charList = new ArrayList<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			charList.add(chars[i]);
+		}
+		List check = new ArrayList();
+		Iterator<String> it = unvisited.iterator();
+		while (it.hasNext()) {
+
+			String currentString = it.next();
+			if (currentString.length() == charList.size()) {
+				List<Character> tempList = new ArrayList<Character>();
+				for (int i = 0; i < charList.size(); i++) {
+					tempList.add(charList.get(i));
+				}
+				for (int i = 0; i < currentString.length(); i++) {
+					// System.out.println(tempList);
+					// System.out.println(currentString.charAt(i));
+					tempList.remove(Character.valueOf(currentString.charAt(i)));
+					System.out.println(tempList);
+					if (tempList.isEmpty() && currentString.length() == i + 1) {
+						System.out.println("anagram");
+
+						queue.add(new Node(currentString, current,
+								current.dist + 1));
+						visited.add(currentString);
+					}
+				}
+
+			}
+		}
 	}
 
 	private static void addPath(List<List<String>> result, Node current) {
